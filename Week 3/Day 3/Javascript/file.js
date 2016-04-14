662f11789c9ea0e03ec73408f93ff2906b12136e
Al Delcy <aldelcy2000@gmail.com>
2016-04-14 11:23:56 -0400

    week 3 update Day 4

---
+++ Week 3/Day 3/Javascript/file.js
@@ -0,0 +1,10 @@
+var exampleArray = [2,-5,10,5,4,-10,0];
+
+var results = process(exampleArray);
+
+function process(data){
+
+	var 
+}
+
+console.log(results);
---
+++ Week 3/Day 3/decoder/decoder.js
@@ -0,0 +1,8 @@
+function decoder (words){
+	var index = 0;
+	var secretMessage = '';
+
+	for (var i=index; i<words.lenght(); i++){
+
+	}
+}	
