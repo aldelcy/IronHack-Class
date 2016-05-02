a7ef78f86eff9fb13f5dc7523ffdf6f50e13ab4c
aldelcy <aldelcy2000@gmail.com>
2016-05-02 12:11:27 -0400

    sandwichr

---
+++ Week 6/Day 1/sandwichr/app/controllers/sandwiches_controller.rb
@@ -0,0 +1,48 @@
+class SandwichesController < ApplicationController
+
+	def index
+		sandwiches = Sandwich.all
+		render json: sandwiches
+	end
+
+	def create
+		sandwich = Sandwich.create(sandwich_params)
+		render json: sandwiches
+	end
+
+	def show
+		sandwich = load_sandwich
+		render json: sandwich
+	end
+
+	def update
+		sandwich = load_sandwich
+		sandwich.update(sandwich_params)
+		render json: sandwich
+	end
+
+	def destroy
+		sandwich = load_sandwich
+		sandwich.destroy
+		render json: sandwich
+	end
+
+
+	private
+
+	def load_sandwich
+		sandwich = Sandwich.find_by(id: params:[:id])
+
+		if sandwich.nil?
+			render json: {error: "sandwich not found"}, status: 404
+			return
+		end
+
+		return sandwich
+	end
+
+	def sandwich_params
+		params.require(:sandwich).permit(:name, :bread_type)
+	end
+
+end
